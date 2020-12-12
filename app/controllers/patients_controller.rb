50d808931ee3816b884d1cf230b740b14c162fd2
anisssoudki <aniss.marrouch@gmail.com>
2020-12-11 17:58:58 -0700

    tried to reset db

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,45 @@
 class PatientsController < ApplicationController
+   
+        before_action :set_patient, only: [:show, :edit, :update, :destroy]
+      
+        def index 
+           @patient = Pateint.all
+           render json: @patient
+        end
+      
+      
+      
+        def show
+            render json: @patient
+        end
+      
+        def create
+            @patient = Patient.find_or_create_by(patient_params)
+            if @patient.valid?
+                render json: @patient
+            else
+                render json: { errors: @patient.errors.full_messages }, status: :unprocessable_entity
+            end
+        end
+      
+        def destroy 
+            
+            @patient.destroy
+          if @patient.valid? 
+            render json: { errors: @patient.errors.full_messages }
+          end
+        end
+      
+        private 
+      
+        def set_patient
+            @patient = patient.find(params[:id]) || Patient.find_by(params[:lastName])
+        end
+      
+        def patient_params
+            params.require(:patient).permit(:firstName, :lastName)
+        end
+      
+      
+    
 end
