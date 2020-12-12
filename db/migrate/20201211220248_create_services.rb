5da8792e7b150ef2ade12e5027cefb5cf690a6d0
anisssoudki <aniss.marrouch@gmail.com>
2020-12-11 17:02:40 -0700

    fixed type

--- db/migrate/20201211220248_create_services.rb
+++ db/migrate/20201211220248_create_services.rb
@@ -2,8 +2,8 @@ class CreateServices < ActiveRecord::Migration[6.0]
   def change
     create_table :services do |t|
       t.string :name
-      t.refrences :CareProvider
-      t.refrences :Patient
+      t.references :CareProviders
+      t.references :Patient
 
       t.timestamps
     end
