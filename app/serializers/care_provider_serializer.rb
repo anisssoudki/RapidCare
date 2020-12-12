8f52a0f3e2e3a69852e5e3fcb98ca50fc0073b2f
anisssoudki <aniss.marrouch@gmail.com>
2020-12-11 18:48:56 -0700

    changed serializer

--- app/serializers/care_provider_serializer.rb
+++ app/serializers/care_provider_serializer.rb
@@ -1,5 +1,4 @@
 class CareProviderSerializer < ActiveModel::Serializer
   attributes :id, :firstName, :lastName, :address, :phoneNumber
-  has_many :services
-  has_many :patients, through: :services
+
 end
--- app/serializers/patient_serializer.rb
+++ app/serializers/patient_serializer.rb
@@ -1,5 +1,4 @@
 class PatientSerializer < ActiveModel::Serializer
   attributes :id, :firstName, :lastName, :address, :phoneNumber
-  has_many :services
-  has_many :care_providers, through: :services
+
 end
